e5578480a231274260342c75a29840cee93e6fd4
Vlasta Vesely <vlastavesely@protonmail.ch>
2017-07-14 18:20:17 +0200

    added HTTP response

---
+++ src/wpbackup-http-response.h
@@ -0,0 +1,15 @@
+#ifndef __WPBACKUP_HTTP_RESPONSE_H
+#define __WPBACKUP_HTTP_RESPONSE_H
+
+struct wpbackup_http_response;
+
+struct wpbackup_http_response *wpbackup_http_response_new(int code, const char *body);
+
+void wpbackup_http_response_free(struct wpbackup_http_response *response);
+
+
+int wpbackup_http_response_get_code(struct wpbackup_http_response *response);
+
+char *wpbackup_http_response_get_body(struct wpbackup_http_response *response);
+
+#endif /* __WPBACKUP_HTTP_RESPONSE_H */
