c270bff728867690c7c7272461df40cefb2a3bf3
Vlasta Vesely <vlastavesely@protonmail.ch>
2017-07-15 09:41:07 +0200

    added global header (wpbackup.h)

---
+++ src/wpbackup.h
@@ -0,0 +1,13 @@
+#ifndef __WPBACKUP_H
+#define __WPBACKUP_H
+
+#define APPNAME "wp-backup"
+#define VERSION "2.0"
+
+#include <wpbackup-http-request.h>
+#include <wpbackup-http-response.h>
+#include <wpbackup-http-client.h>
+#include <wpbackup-password-resolver.h>
+#include <wpbackup-debug.h>
+
+#endif /* __WPBACKUP_H */
