bf95e4be7cf08320969e89d7ba51681329af9b3a
Vlasta Vesely <vlastavesely@protonmail.ch>
2017-07-16 16:12:26 +0200

    debug: turn on debugger by define __DEBUG

--- include/wpbackup/debug.h
+++ include/wpbackup/debug.h
@@ -1,7 +1,11 @@
 #ifndef __DEBUG_H
 #define __DEBUG_H
 
-#define DEBUG(format, args...) debug_info(format, args)
+#ifdef __DEBUG
+	#define DEBUG(format, args...) debug_info(format, args)
+#else
+	#define DEBUG(format, args...)
+#endif
 
 void debug_info(char *format,...);
 
