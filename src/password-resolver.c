47aaec5dcc464c9543db6c13b65c01c4412d9efc
Vlasta Vesely <vlastavesely@protonmail.ch>
2017-10-14 09:44:20 +0200

    password resolver: refactoring

--- src/password-resolver.c
+++ src/password-resolver.c
@@ -23,21 +23,18 @@
 
 #include <wp-backup/password-resolver.h>
 
-#define PASSWORD_BUFFER_SIZE 256
-
-static void print_prompt()
+static void print_password_prompt()
 {
 	fprintf(stderr, "Enter your WordPress password: ");
 }
 
-static void trim_trailing_newlines(char **str)
+static void trim_trailing_newlines(char *str)
 {
 	char *end;
 
-	end = *str + strlen(*str) - 1;
-	while(end > *str && ((char) *end == '\n' || (char) *end == '\r')) {
+	end = str + strlen(str) - 1;
+	while(end > str && ((char) *end == '\n' || (char) *end == '\r'))
 		end--;
-	}
 	end++;
 
 	*end = 0;
@@ -45,43 +42,39 @@ static void trim_trailing_newlines(char **str)
 
 char *password_resolver_resolve_password()
 {
-	char *password = getenv("WPPASS");
-	if (password) {
+	struct termios oflags, nflags;
+	char buffer[256];
+	char *password;
+
+	password = getenv("WPPASS");
+	if (password)
 		return strdup(password);
-	} else {
-		char *buffer = malloc(PASSWORD_BUFFER_SIZE);
-		memset(buffer, 0, PASSWORD_BUFFER_SIZE);
 
-		if (isatty(0)) {
-			struct termios oflags;
-			struct termios nflags;
+	if (isatty(0)) {
+		tcgetattr(0, &oflags);
+		nflags = oflags;
+		nflags.c_lflag &= ~ECHO;
+		nflags.c_lflag |= ECHONL;
 
-			tcgetattr(0, &oflags);
-			nflags = oflags;
-			nflags.c_lflag &= ~ECHO;
-			nflags.c_lflag |= ECHONL;
-
-			if (tcsetattr(0, TCSANOW, &nflags) != 0) {
-				perror("tcsetattr");
-				return NULL;
-			}
-
-			print_prompt();
-
-			fgets(buffer, PASSWORD_BUFFER_SIZE, stdin);
-			if (tcsetattr(0, TCSANOW, &oflags) != 0) {
-				perror("tcsetattr");
-				return NULL;
-			}
-		} else {
-			fgets(buffer, PASSWORD_BUFFER_SIZE, stdin);
+		if (tcsetattr(0, TCSANOW, &nflags) != 0) {
+			perror("tcsetattr");
+			return NULL;
 		}
 
-		trim_trailing_newlines(&buffer);
-		password = malloc(strlen(buffer) + 1);
-		strcpy(password, buffer);
-		memset(buffer, 0, PASSWORD_BUFFER_SIZE);
+		print_password_prompt();
 
-		return password;
+		fgets(buffer, sizeof buffer, stdin);
+		if (tcsetattr(0, TCSANOW, &oflags) != 0) {
+			perror("tcsetattr");
+			return NULL;
+		}
+	} else {
+		fgets(buffer, sizeof buffer, stdin);
 	}
+
+	trim_trailing_newlines(buffer);
+	password = strdup(buffer);
+	memset(buffer, 0, sizeof buffer);
+
+	return password;
 }
