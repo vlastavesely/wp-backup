ca035b93d01cf8f268f51c768a3d2e259863a9fb
Vlasta Vesely <vlastavesely@protonmail.ch>
2017-10-28 13:04:47 +0200

    added error structure

---
+++ tests/test-error.c
@@ -0,0 +1,24 @@
+#include <CUnit/CUnit.h>
+
+#include "../src/error.h"
+
+static void test_error_error()
+{
+	struct error *error;
+
+	error = error_new(11, "This is a error.");
+	CU_ASSERT_EQUAL(11, error->code);
+	CU_ASSERT_STRING_EQUAL("This is a error.", error->message);
+	error_free(error);
+
+	error = error_new(44, "%d + %d is not %d.", 2, 2, 5);
+	CU_ASSERT_EQUAL(44, error->code);
+	CU_ASSERT_STRING_EQUAL("2 + 2 is not 5.", error->message);
+	error_free(error);
+}
+
+void test_error_add_tests(struct CU_Suite *suite)
+{
+	CU_add_test(suite, "test_error_error",
+		test_error_error);
+}
---
+++ tests/test-error.h
@@ -0,0 +1,6 @@
+#ifndef __TEST_ERROR_H
+#define __TEST_ERROR_H
+
+void test_error_add_tests(struct CU_Suite *suite);
+
+#endif /* __TEST_ERROR_H */
