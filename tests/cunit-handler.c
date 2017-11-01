7a6a4e6b9cbeb17e24c367e7c09c04180d985993
Vlasta Vesely <vlastavesely@protonmail.ch>
2017-11-01 21:14:59 +0100

    cunit handler: implemented show_failures()

--- tests/cunit-handler.c
+++ tests/cunit-handler.c
@@ -7,5 +7,12 @@ CU_ErrorCode CU_custom_run_tests(void)
 
 void CU_custom_show_failures(struct CU_FailureRecord *failure)
 {
+	int i = 1;
 
+	while (failure) {
+		fprintf(stdout, "  %d. %s:%d  - %s\n", i++,
+			failure->strFileName, failure->uiLineNumber,
+			failure->strCondition);
+		failure = failure->pNext;
+	}
 }
