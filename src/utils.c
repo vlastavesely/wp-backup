fe075b3b8b9b2a596dc17a2c7630a5dd1aaa35a7
Vlasta Vesely <vlastavesely@protonmail.ch>
2017-12-17 15:54:28 +0100

    cs

--- src/utils.c
+++ src/utils.c
@@ -54,17 +54,17 @@ static void pack_unicode_char(char **dest, unsigned long c)
 	if (c <= 0x00007f) {
 		*(*dest)   = c;
 	} else if (c <= 0x0007ff) {
-		*(*dest)++ = (0xc0 |  (c >> 6)         );
-		*(*dest)   = (0x80 |  (c        & 0x3f));
+		*(*dest)++ = (0xc0 | ((c >>  6) & 0xff));
+		*(*dest)   = (0x80 | ((c >>  0) & 0x3f));
 	} else if (c <= 0x00ffff) {
-		*(*dest)++ = (0xe0 |  (c >> 12)        );
-		*(*dest)++ = (0x80 | ((c >> 6)  & 0x3f));
-        	*(*dest)   = (0x80 |  (c        & 0x3f));
+		*(*dest)++ = (0xe0 | ((c >> 12) & 0xff));
+		*(*dest)++ = (0x80 | ((c >>  6) & 0x3f));
+        	*(*dest)   = (0x80 | ((c >>  0) & 0x3f));
 	} else if (c <= 0x10ffff) {
-		*(*dest)++ = (0xf0 |  (c >> 18)        );
+		*(*dest)++ = (0xf0 | ((c >> 18) & 0xff));
 		*(*dest)++ = (0x80 | ((c >> 12) & 0x3f));
-		*(*dest)++ = (0x80 | ((c >> 6)  & 0x3f));
-		*(*dest)   = (0x80 |  (c        & 0x3f));
+		*(*dest)++ = (0x80 | ((c >>  6) & 0x3f));
+		*(*dest)   = (0x80 | ((c >>  0) & 0x3f));
 	}
 }
 
