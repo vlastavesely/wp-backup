af903e72151737b9129b737413151c7ba4d22fb5
Vlasta Vesely <vlastavesely@protonmail.ch>
2017-12-21 17:43:25 +0100

    added compat.h

---
+++ src/compat.h
@@ -0,0 +1,19 @@
+#ifndef __COMPAT_H
+#define __COMPAT_H
+
+#include "../config.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <assert.h>
+#include <termios.h>
+#include <errno.h>
+#include <ctype.h>
+
+#endif /* __COMPAT_H */
